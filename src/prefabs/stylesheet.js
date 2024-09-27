eed3620be02d39689768232375ffb4d10718507c
Rob Dekker <rob-ajax@live.nl>
2024-09-27 10:28:34 +0200

    feat: add custom stylesheet component with variable option

---
+++ src/prefabs/stylesheet.js
@@ -0,0 +1,20 @@
+(() => ({
+  name: 'Custom Style Sheet',
+  icon: 'HtmlIcon',
+  category: 'STYLING',
+  keywords: ['Stylesheet', 'styling'],
+  structure: [
+    {
+      name: 'Custom Style Sheet',
+      options: [
+        {
+          type: 'VARIABLE',
+          label: 'Stylesheet URL',
+          key: 'styleSheetUrl',
+          value: [''],
+        },
+      ],
+      descendants: [],
+    },
+  ],
+}))();
