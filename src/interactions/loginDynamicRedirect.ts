8c087fc039552da83626fa044a2d8e0e17332232
Rob Dekker <rob-ajax@live.nl>
2025-10-02 13:54:12 +0200

    feat: add login interaction with dynamic redirect

---
+++ src/interactions/loginDynamicRedirect.ts
@@ -0,0 +1,45 @@
+/* eslint @typescript-eslint/no-unused-vars: ["error", { "varsIgnorePattern": "login" }] */
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+interface History {
+  push(url: string): string;
+}
+
+interface Event {
+  isValid: boolean;
+  jwtToken: string;
+  refreshToken: string;
+  redirectUrl: string;
+}
+
+interface Page {
+  name: string;
+  url: string;
+}
+
+function loginDynamicRedirect({
+  event,
+  redirectTo,
+}: {
+  event: Event;
+  redirectTo: Page;
+}): void {
+  const { isValid, jwtToken, refreshToken, redirectUrl } = event;
+  const { url } = redirectTo;
+
+  if (isValid) {
+    const decodedToken = JSON.parse(window.atob(jwtToken.split('.')[1]));
+    if (decodedToken && decodedToken.locale) {
+      document.cookie = `BBLocale=${decodedToken.locale};path=/`;
+    }
+
+    const d = new Date(0);
+    const e = d.toUTCString();
+    document.cookie = `betty_jwt=none; expires=${e}; SameSite=Lax; path=/`;
+
+    localStorage.setItem('TOKEN', jwtToken);
+    localStorage.setItem('REFRESH_TOKEN', refreshToken);
+    // eslint-disable-next-line no-restricted-globals
+    history.push(redirectUrl || url);
+  }
+}
